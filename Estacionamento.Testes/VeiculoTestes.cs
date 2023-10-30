fdfdcb705a27de868396baf6aa14f20038805612
Kathelyn Gaioni <112026988+kathelyngaioni@users.noreply.github.com>
2023-10-30 14:47:15 -0300

    padrão AAA

--- Estacionamento.Testes/VeiculoTestes.cs
+++ Estacionamento.Testes/VeiculoTestes.cs
@@ -1,12 +1,31 @@
 namespace Estacionamento.Testes;
 
+/* Padrão AAA
+Arrange -> organizar
+Act -> agir
+Assert -> afirmar
+*/
+
 public class VeiculoTestes
 {
     [Fact]
     public void TestaVeiculoAcelerar()
     {
+        //Arrange
         var veiculo = new Veiculo();
+
+        //Act
         veiculo.Acelerar(10);
+
+        //Assert
         Assert.Equal(100,veiculo.VelocidadeAtual); //resultado esperado
+    }
+
+    [Fact]
+    public void TestaVeiculoFrear()
+    {
+        var veiculo = new Veiculo();
+        veiculo.Frear(10);
+        Assert.Equal(-150,veiculo.VelocidadeAtual); //resultado esperado
     }
 }
